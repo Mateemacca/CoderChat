bb3dd4a0353f858a15360caa4eadea265bd23df4
Mate Maccarone <mateobauti.maccarone@hotmail.com>
2023-12-14 20:37:42 -0300

    coderchat

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+const socket = io()
+
+let userName;
+
+Swal.fire({
+    title: 'Ingrese su nombre',
+    input: 'text',
+    inputValidator:(value)=>{
+        if(!value) return "Por favor ingrese un nombre"
+    }
+}).then(data=>{
+    userName=data.value
+    socket.emit('newUser',userName)
+})
+
+const inputData = document.getElementById('inputData')
+const outputData = document.getElementById('outputData')
+
+inputData.addEventListener('keyup',(event)=>{
+    if(event.key === 'Enter'){
+        if(inputData.value.trim().length > 0){
+            socket.emit('message',{user:userName,data:inputData.value})
+        }
+    }
+})
+
+socket.on('messageLogs', data=>{
+    let messages = ''
+    data.forEach(message => {
+        messages +=`${message.user} dice: ${message.data}<br />`
+    });
+
+    outputData.innerHTML = messages;
+})
+
+socket.on('newConnection',data=>{
+    console.log(data)
+})
+socket.on('notification',user=>{
+    Swal.fire({
+        text:`${user} se conecto`,
+        toast:true,
+        position:'top-right'
+    })
+})
